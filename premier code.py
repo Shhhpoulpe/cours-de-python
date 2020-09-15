3d3f4105cff97ecfc07e8c117501f9fa6ea7d21a
Shhhpoulpe <max.jobard@hotmail.fr>
2020-09-15 16:27:12 +0200

    remove comment

--- premier code.py
+++ premier code.py
@@ -1,52 +1,52 @@
-# # Exo 1
+# Exo 1
 
-# a = 5
-# b = 10
+a = 5
+b = 10
 
-# c=a
-# a=b
-# b=c
+c=a
+a=b
+b=c
 
-# print("a = " , a , ", b =" , b)
+print("a = " , a , ", b =" , b)
 
-# # Exo 2
+# Exo 2
 
-# entree = int(input("Veuillez entrer une valeur"))
-# print(entree*entree)
+entree = int(input("Veuillez entrer une valeur"))
+print(entree*entree)
 
-# # Exo 3
+# Exo 3
 
-# entree = int(input("Veuillez entrer une valeur"))
+entree = int(input("Veuillez entrer une valeur"))
 
-# if entree > 0:
-#     print("Positif")
-# else:
-#     print("négatif")
+if entree > 0:
+    print("Positif")
+else:
+    print("négatif")
 
-# # Exo 4
+# Exo 4
 
-# entree = int(input("Veuillez entrer une valeur"))
-# entree2 = int(input("Veuillez entrer une autre valeur"))
+entree = int(input("Veuillez entrer une valeur"))
+entree2 = int(input("Veuillez entrer une autre valeur"))
 
-# if entree + entree2 > 0:
-#     print("Positif")
-# else:
-#     print("négatif")
+if entree + entree2 > 0:
+    print("Positif")
+else:
+    print("négatif")
 
-# # Exo 5
+# Exo 5
 
-# entree = int(input("Veuillez entrer une valeur"))
+entree = int(input("Veuillez entrer une valeur"))
 
-# for i in range(10):
-#     print(entree + i + 1)
+for i in range(10):
+    print(entree + i + 1)
 
-# # Exo 6
+# Exo 6
 
-# entree = int(input("Veuillez entrer une valeur"))
-# res = 0
-# for i in range(entree):
-#     res += i + 1
-# print(res)
+entree = int(input("Veuillez entrer une valeur"))
+res = 0
+for i in range(entree):
+    res += i + 1
+print(res)
 
 # Exo 2.1
 
