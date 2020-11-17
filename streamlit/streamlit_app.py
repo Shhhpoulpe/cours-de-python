aca6c766bb93c77cf9dacbf4783a85e9db3ebda8
unknown <max.jobard@hotmail.fr>
2020-11-17 14:45:21 +0100

    affichage de deux datasets

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,17 +4,38 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 
-DATE_TIME = "date/time"
-DATA_URL = (
-    "csv/Video_Games_Sales.csv"
+video_games_URL = (
+    "streamlit/csv/Video_Games_Sales.csv"
+)
+
+netflix_URL = (
+    "streamlit/csv/netflix_titles.csv"
 )
 
 @st.cache(persist=True)
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+def load_video_games(nrows):
+    data = pd.read_csv(video_games_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis="columns", inplace=True)
-    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
     return data
 
-data = load_data(100000)
+@st.cache(persist=True)
+def load_netflix(nrows):
+    data = pd.read_csv(netflix_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    return data
+
+st.title("Découverte de streamlib")
+
+nb_video_games = st.number_input('Nombre de ligne a afficher pour les jeux',value=5,step=1)
+data_video_games = load_video_games(nb_video_games)
+nb_video_games
+
+'video games', data_video_games
+
+nb_netflix = st.number_input('Nombre de ligne a afficher pour netflix',value=5,step=1)
+data_netflix = load_netflix(nb_netflix)
+nb_netflix
+
+'netflix', data_netflix
