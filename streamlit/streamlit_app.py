5246132a8faf6021981b3cdd7fd949c4c2205029
unknown <max.jobard@hotmail.fr>
2020-11-17 15:08:59 +0100

    update

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -27,6 +27,7 @@ def load_netflix(nrows):
     return data
 
 st.title("Découverte de streamlib")
+st.title("Affichage des tableaux")
 
 nb_video_games = st.number_input('Nombre de ligne a afficher pour les jeux',value=5,step=1)
 data_video_games = load_video_games(nb_video_games)
@@ -39,3 +40,19 @@ data_netflix = load_netflix(nb_netflix)
 nb_netflix
 
 'netflix', data_netflix
+
+st.title("Affichage des colonnes")
+
+data_video_games.columns
+
+st.title("Affichage des types des colonnes et des selections")
+
+list_result = st.selectbox(
+    'test list',
+    (data_video_games.columns))
+
+st.write('Vous avez choisi:', list_result, ' qui est de type', data_video_games[list_result].dtype)
+
+st.title("Affichage de la shape du dataset")
+
+data_video_games.shape
