271f9a7c4759d0d67b0a3c847d2e923886f0d16c
unknown <max.jobard@hotmail.fr>
2020-12-03 14:40:52 +0100

    update

--- mini projet/app.py
+++ mini projet/app.py
@@ -3,15 +3,24 @@ from flask import request
 from jinja2 import Template, FileSystemLoader, Environment
 import json
 
+"""
+L'application web python va permettre aux joueurs de GTA
+d'avoir un accès facile au nouveauté de la semaine sur
+la page internet
+"""
+
 app = Flask(__name__)
 
-# Ouverture du template
-file_loader = FileSystemLoader('templates')
-env = Environment(loader=file_loader)
-template = env.get_template('index.jinja2.html')
 
-def index_fun():
-    result = template.render(info=get_weekly_info("Discounts","Cars"))
+
+def index_fun(categ1 = "all", categ2 = None):
+    # Ouverture du template
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader)
+    template = env.get_template('index.jinja2.html')
+    result = template.render(info=get_weekly_info(categ1,categ2),
+                             categ1 = categ1,
+                             categ2 = categ2)
     return result
 
 def get_weekly_info(categ1 = "all", categ2 = None):
@@ -34,6 +43,36 @@ def get_weekly_info(categ1 = "all", categ2 = None):
 @app.route('/')
 def index():
     response = index_fun()
+    return response
+
+@app.route('/podium')
+def podium():
+    response = index_fun("Podium")
+    return response
+
+@app.route('/login')
+def login():
+    response = index_fun("Log in")
+    return response
+
+@app.route('/double')
+def double_money():
+    response = index_fun("Double money")
+    return response
+
+@app.route('/triple')
+def triple_money():
+    response = index_fun("Triple money")
+    return response
+
+@app.route('/discounts/cars')
+def discounts_cars():
+    response = index_fun("Discounts","Cars")
+    return response
+
+@app.route('/discounts/properties')
+def discounts_Properties():
+    response = index_fun("Discounts","Properties")
     return response
 
 if __name__ == '__main__':
