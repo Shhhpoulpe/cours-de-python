eb489eb181fcef4aec189790fe57e3019bea29cb
root <max.jobard@hotmail.fr>
2020-12-05 18:59:31 +0100

    fin mini projet

--- mini projet/app.py
+++ mini projet/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from jinja2 import Template, FileSystemLoader, Environment
 import json
+import os
 
 """
 L'application web python va permettre aux joueurs de GTA
@@ -74,4 +75,4 @@ def discounts_Properties():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=os.environ.get('PORT',5000))
